7cc54f07ca6f97cb6c5e2b07dd85a88c3f5bd22e
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-08-22 02:38:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const pickMeUp = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh'
+  },
+  {
+    name: 'Sammy'
+    hometown: 'New York'
+  },
+  {
+    name: 'Sally',
+    hometown: 'cleveland'
+  },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles'
+  },
+  {
+    name: 'Bobby'
+    hometown: 'Tampa Bay'
+  }
+
+];
+function findMatching(collection, string) {
+  let f = collection.filter(function){
+    if(name.toLowerCase()===string.toLowerCase)
+return (['name'])  }
+}
